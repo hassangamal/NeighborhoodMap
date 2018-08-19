f3da00b16d68096daaeb629ab31e39da1df71487
hassangamal <gamalhassan807@yahoo.com>
2018-08-20 01:32:43 +0200

    second

--- js/app.js
+++ js/app.js
@@ -43,78 +43,84 @@ var dblocations = [{
 
 
 var markers = [];
-
+var map;
 function showMarker(locations) {
-    var map = new google.maps.Map(document.getElementById('map'), {
+    map = new google.maps.Map(document.getElementById('map'), {
         zoom: 12,
-        center: { 
-                lat: 40.7413549,
-                lng: -73.9980244
-                }
+        center: {
+            lat: 40.7413549,
+            lng: -73.9980244
+        }
     });
     for (var i = 0; i < locations.length; i++) {
-        var position = locations[i].location;
-        var title = locations[i].title;
-       
-        var marker = new google.maps.Marker({
-            position: position,
-            map:map,
-            title: title,
-            animation: google.maps.Animation.DROP,
-            id:i
-        });
-        var content = "";
-        var placesAPI ="https://api.foursquare.com/v2/venues/search?" +
-    "client_id=" + clientId + "&client_secret=" + clientSecret +
-    "&v=20180323"+"&limit=1&ll="+position.lat+","+position.lng+"&query=" + title;
-    $.ajax({
-        url: placesAPI,
-        dataType: 'json',
-        async:false,
-        success:function(data){
-            var obj=data.response.venues[0]; 
-            console.log(obj);
-            if(obj){
-            content+='<p>information about'+title+'</p>'+
-            '</br> <p>location :'+ obj.location.lat +" , "+ obj.location.lat+'</p></br> '+
-            '</br> <p>Adress :'+obj.location.address+'</p></br> '+
-            '</br> <p>country :'+obj.location.country+'</p></br> ';
-            }
-            else
-            {
-            content+='<p>information about'+title+'</p>'+
-            '</br> <p>No Informantion about this location yet.</p></br> ';
-            }
-        },
-        fail:function(){
-            content='Fail To Load Data please check palce or this palce not found'
-        }
-         });
-        var infowindow = new google.maps.InfoWindow();
-        google.maps.event.addListener(marker,'click', (function(marker,content,infowindow){ 
-            return function() {
-                infowindow.setContent(content);
-                infowindow.open(map,marker);
-            };
-        })(marker,content,infowindow)); 
+        show_location(map, locations[i]);
     }
- 
 }
-var ViewModel = function() {
+function show_location(map, location) {
+    var position = location.location;
+    var title = location.title;
+
+    var marker = new google.maps.Marker({
+        position: position,
+        map: map,
+        title: title,
+        animation: google.maps.Animation.DROP,
+    });
+    var content = "";
+    // foursquare api and call it
+    var placesAPI = "https://api.foursquare.com/v2/venues/search?" +
+        "client_id=" + clientId + "&client_secret=" + clientSecret +
+        "&v=20180323" + "&limit=1&ll=" + position.lat + "," + position.lng + "&query=" + title;
+    var request = $.ajax({
+        url: placesAPI,
+        method: "GET",
+        dataType: "json",
+        async: false
+    });
+    request.done(function (data) {
+        var obj = data.response.venues[0];
+        if (obj) {
+            content += '<p>information about' + title + '</p>' +
+                '</br> <p>location :' + obj.location.lat + " , " + obj.location.lat + '</p></br> ' +
+                '</br> <p>Adress :' + obj.location.address + '</p></br> ' +
+                '</br> <p>country :' + obj.location.country + '</p></br> ';
+        }
+        else {
+            content += '<p>information about' + title + '</p>' +
+                '</br> <p>No Informantion about this location yet.</p></br> ';
+        }
+    });
+    request.fail(function () {
+        content = 'Fail To Load Data please check palce or this palce not found';
+    });
+    show_infowindow(marker, content);
+    var toggleBounce = new google.maps.Marker();
+    google.maps.event.addListener(marker, 'click', (function (marker, toggleBounce) {
+        return function () {
+            if (marker.getAnimation() !== null) {
+                marker.setAnimation(null);
+            } else {
+                marker.setAnimation(google.maps.Animation.BOUNCE);
+            }
+        };
+    })(marker, toggleBounce));
+    // Setting trigger Bounce event marker to call it from the view
+}
+var ViewModel = function () {
     var self = this;
     self.txt_search = ko.observable("");
     self.locations = ko.observableArray();
-    self.location_search = ko.computed(function() {
+    self.location_search = ko.computed(function () {
         var search = self.txt_search().toLowerCase();
         self.locations.removeAll();
         if (!search) {
-            dblocations.forEach(function(element) {
+            dblocations.forEach(function (element) {
                 self.locations.push(element);
             });
             showMarker(self.locations());
             return self.locations();
         } else {
-            dblocations.forEach(function(element) {
+            dblocations.forEach(function (element) {
                 var titleorg = element.title.toLowerCase();
                 if (titleorg.search(search) >= 0) {
                     self.locations.push(element);
@@ -125,7 +131,36 @@ var ViewModel = function() {
         }
     });
 };
+function selectitem(obj) {
+    console.log(obj);
+    var content = "";
+    content += '<p>information about' + obj.title + '</p>' +
+        '</br> <p>location :' + obj.location.lat + " , " + obj.location.lat + '</p></br> ' ;
+
+    var marker = new google.maps.Marker({
+        position: obj.location,
+        map: map,
+        title: obj.title,
+        animation: google.maps.Animation.DROP,
+    });
+    var infowindow = new google.maps.InfoWindow();
+    infowindow.setContent(content);
+    infowindow.open(map, marker);
+}
+function show_infowindow(marker, content) {
+    var infowindow = new google.maps.InfoWindow();
+    google.maps.event.addListener(marker, 'click', (function (marker, content, infowindow) {
+        return function () {
+            infowindow.setContent(content);
+            infowindow.open(map, marker);
+        };
+    })(marker, content, infowindow));
+}
 
 function initMap() {
     ko.applyBindings(new ViewModel());
 }
+
+myerrorhandler = function () {
+    alert("This page cannot be loaded sucessfully.");
+};
